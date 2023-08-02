ab7d9c9829064a7c5b128baadb9b1c3d3427ddb9
abtsty <andrey.barylov@gmail.com>
2023-08-02 17:57:20 +0300

    add yachts swiper section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,26 @@
-const swiper = new Swiper('.swiper', { 
-    // Optional parameters 
-    direction: 'horizontal', 
-    loop: true, 
-   
-    // Navigation arrows 
-    navigation: { 
-      nextEl: '.swiper-button-next', 
-      prevEl: '.swiper-button-prev', 
-    }, 
-  }); 
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+  },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+});
